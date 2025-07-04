7b5c60e4dc7e5165741b7179ec03066ced62656c
Andrei Hava <andrei.hava@posteo.de>
2025-07-04 15:27:43 +0300

    inital commit

---
+++ wezterm/wezterm.lua
@@ -0,0 +1,23 @@
+-- vim: ts=2 sts=2 sw=2 et
+local wezterm = require("wezterm") --[[@as Wezterm]]
+local config = wezterm.config_builder()
+
+-- Set rendering settings
+config.animation_fps = 30
+
+-- Set theme
+config.color_scheme = "Tokyo Night"
+config.window_background_opacity = 0.8
+-- config.font = wezterm.font("FiraCode Nerd Font Mono")
+config.font_size = 10.0
+
+-- Disable tab bar
+config.enable_tab_bar = false
+
+-- Don't warn about missing glyphs
+config.warn_about_missing_glyphs = false
+
+-- Don't ask when closing
+config.window_close_confirmation = "NeverPrompt"
+
+return config
